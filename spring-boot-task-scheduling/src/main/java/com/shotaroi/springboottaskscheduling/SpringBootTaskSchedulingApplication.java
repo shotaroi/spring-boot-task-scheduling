4586a535e8151a05baa85acc152c7fbf01e70655
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-01-14 00:17:57 +0100

    Create project

---
+++ spring-boot-task-scheduling/src/main/java/com/shotaroi/springboottaskscheduling/SpringBootTaskSchedulingApplication.java
@@ -0,0 +1,13 @@
+package com.shotaroi.springboottaskscheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTaskSchedulingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTaskSchedulingApplication.class, args);
+	}
+
+}
---
+++ spring-boot-task-scheduling/src/test/java/com/shotaroi/springboottaskscheduling/SpringBootTaskSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shotaroi.springboottaskscheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTaskSchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
