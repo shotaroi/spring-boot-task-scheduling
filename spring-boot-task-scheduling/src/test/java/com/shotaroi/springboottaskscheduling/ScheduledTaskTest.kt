fddfdabd07cff4585f254f0a30ab84c743bbe03b
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-01-14 00:30:08 +0100

    Update ScheduledTaskTest.kt

--- spring-boot-task-scheduling/src/test/java/com/shotaroi/springboottaskscheduling/ScheduledTaskTest.kt
+++ spring-boot-task-scheduling/src/test/java/com/shotaroi/springboottaskscheduling/ScheduledTaskTest.kt
@@ -1,4 +1,17 @@
 package com.shotaroi.springboottaskscheduling
 
+import org.awaitility.Durations
+import org.junit.jupiter.api.Test
+//import org.springframework.boot.test.mock.mockito.SpyBean
+
 class ScheduledTaskTest {
+//    @SpyBean
+//    ScheduledTasks tasks;
+//
+//    @Test
+//    public void reportCurrentTime() {
+//        await().atMost(Durations.TEN_SECONDS).untilAsserted(() -> {
+//            verify(tasks, atLeast(2)).reportCurrentTime();
+//        });
+//    }
 }
